70b9a6e754110ee7627e64f0ec4ab3e8de663fe0
miquel-manzano <miquel.manzano.7e6@itb.cat>
2024-12-05 12:58:37 +0100

    exercici2 FInish

---
+++ ArraySearch/Class1.cs
@@ -0,0 +1,57 @@
+﻿namespace ArraySearch
+{
+    public class SearchNumArray
+    {
+        public static int BinarySearch(int[] arr, int first, int last, int key)
+        {
+            if (last >= first)
+            {
+                int mid = first + (last - first) / 2;
+                if (arr[mid] == key)
+                {
+                    return mid;
+                }
+                if (arr[mid] > key)
+                {
+                    return BinarySearch(arr, first, mid - 1, key);//cerca en el subarray esquerre
+                }
+                else
+                {
+                    return BinarySearch(arr, mid + 1, last, key);//cerca en el subarray dret
+                }
+            }
+            return -1;
+        }
+    }
+    public class SortArray
+    {
+        public static void Complete(int[] v, int left, int m, int right)
+        {
+            int n = right - left + 1;
+            int[] aux = new int[n];
+            int k = 0;
+            int i = left;
+            int j = m + 1;
+            while (i <= m && j <= right)
+            {
+                if (v[i] < v[j]) { aux[k] = v[i]; ++i; }
+                else { aux[k] = v[j]; ++j; }
+                ++k;
+            }
+            while (i <= m) { aux[k] = v[i]; ++i; ++k; }
+            while (j <= right) { aux[k] = v[j]; ++j; ++k; }
+            for (k = 0; k < n; ++k) v[left + k] = aux[k];
+        }
+
+        public static void Order(int[] v, int left, int right)
+        {
+            if (left < right)
+            {
+                int m = (left + right) / 2;
+                Order(v, left, m);
+                Order(v, m + 1, right);
+                Complete(v, left, m, right);
+            }
+        }
+    }
+}
--- projecteProva2/Program.cs
+++ projecteProva2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        const int LeftArrayLimit = 0;
+        const int RightArrayLimit = 7;
+        const int SearchedNum = 10;
+        int resultat;
+
+        int[] ValidNumList = { 10, 4, 6, 4, 8, -13, 2, 3 };
+        ArraySearch.SortArray.Order(ValidNumList, LeftArrayLimit, RightArrayLimit);
+        resultat = ArraySearch.SearchNumArray.BinarySearch(ValidNumList, LeftArrayLimit, RightArrayLimit, SearchedNum) + 1;
+        Console.WriteLine($"El numero {SearchedNum} es a la posicio: {resultat} sense contar el 0.");
+    }
+}
+
+/*demani un valor enter a l'usuari per teclat i aquest retorni si es troba o no a la llista següent:
+ { 10, 4, 6, 4, 8, -13, 2, 3} (1,5 punts)*/
